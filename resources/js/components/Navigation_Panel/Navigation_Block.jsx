6603b85bde2cd69e8148bff9f780f191355e958c
melvin-rulit <marchenko1985g@outlook.com>
2023-04-05 19:22:31 +0300

    add scss and modification Navigation_Block

--- resources/js/components/Navigation_Panel/Navigation_Block.jsx
+++ resources/js/components/Navigation_Panel/Navigation_Block.jsx
@@ -1,38 +1,41 @@
-import React, {Component} from "react";
-import List_menu_navigation from './list_menu_block'
-import Dop_right_instrument_block from './dop_right_instruments'
-import Dop_right_instrument_block_2 from './dop_right_instruments_2'
-import Dop_right_instrument_block_3 from './dop_right_instruments_3'
+import React, { Component } from "react";
+import "./navigation_panel.scss";
+
+import List_menu_navigation from "./list_menu_block";
+
 
 export default class navigation_Block extends Component {
-
     render() {
         return (
+            <div className={"navigation_section"}>
+                <div className={"logotip_block"}>
+                    <div>Cars CHOP</div>
+                </div>
 
-                <div className={'section'}>
+                <div className={"list_menu_navigation"}>
+                    {<List_menu_navigation />}
+                </div>
 
-                    <div className={'logotip_block'}>
-                        <div>Cars CHOP</div>
+                <div className={"dop_instrument_block"}>
+                    <div className={"search"}>
+                        <input placeholder={"find you car from USA"} />
                     </div>
+                </div>
 
-                    <div className={'list_menu_navigation'}>
-                        {<List_menu_navigation />}
-                    </div>
-
-                    <div className={'fg'}>
-                        {<Dop_right_instrument_block />}
-                    </div>
-
-                    <div>
-                        {<Dop_right_instrument_block_2 />}
-                    </div>
-
-                    <div>
-                        {<Dop_right_instrument_block_3 />}
+                <div className={"dop_instrument_block"}>
+                    <div className={"cart"}>
+                        <img src="images/Navigation Panel/shopping-cart.png" />
                     </div>
 
                 </div>
 
-        )
+                <div className={"dop_instrument_block"}>
+                    <div className={"car"}>
+
+                    </div>
+                </div>
+
+            </div>
+        );
     }
 }
