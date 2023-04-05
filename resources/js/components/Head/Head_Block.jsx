4b285d163b2580bc96cde46994def74496b000b7
melvin-rulit <marchenko1985g@outlook.com>
2023-04-05 19:28:54 +0300

    add scss for Head Block

--- resources/js/components/Head/Head_Block.jsx
+++ resources/js/components/Head/Head_Block.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import "./head_block.scss"
+
 import Block_1_Head from './block_one'
 import Block_2_Head from './block_two'
 
@@ -9,7 +10,7 @@ export default class Head_Block extends Component {
     render() {
         return (
 
-            <div className={'section'}>
+            <div className={'head_section'}>
 
                 <div className={'left_dop_menu'}>
                     <Block_1_Head/>
